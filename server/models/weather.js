00fdae99a452edee895ac27855708e69d2b1aad7
Alpesh Jadav - Omkar <alpeshjadav11899@gmail.com>
2020-05-25 05:54:41 -0700

    Update weather.js

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,11 +1,11 @@
 const request = require('request-promise');
 
-const API_KEY = '';
+require('dotenv').config();
 
 class Weather {
     static retriveByCity (city, callback) {
         request({
-            uri:`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            uri:`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env. WEATHER_API_KEY}&units=imperial`,
             json: true
         }).then(function (res) {
             callback(res);
